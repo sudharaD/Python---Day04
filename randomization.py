81aa6f4742ebc80c4182593711dbe9ca3b428fa9
sudharaD <sudharad7@gmail.com>
2022-12-20 23:52:34 +0530

    randomization start 5

--- randomization.py
+++ randomization.py
@@ -11,3 +11,7 @@ random_float = random.random()
 print(random_float)
 
 print(random_number + random_float)
+print(random_float * 5)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}%")
