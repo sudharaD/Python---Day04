6d77e515ffddcd139ea339d5d967cb3d021b325e
sudharaD <sudharad7@gmail.com>
2022-12-20 23:49:22 +0530

    randomization start 4

--- randomization.py
+++ randomization.py
@@ -1,7 +1,13 @@
 import random
 import module_example
 
-random_number = random.randint(1, 10)
+random_number = random.randint(1, 4)
 
 print(random_number)
 print(module_example.pi)
+
+random_float = random.random()
+
+print(random_float)
+
+print(random_number + random_float)
