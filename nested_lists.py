056f9ebc4d0b3e2b3f14ec9746d3e384daab187f
sudharaD <sudharad7@gmail.com>
2022-12-21 22:59:51 +0530

    nested lists

---
+++ nested_lists.py
@@ -0,0 +1,6 @@
+list_1 = ["Carrots", "broccoli", "spinach", "kale", "bell peppers", "onions"]
+list_2 = ["potatoes", "sweet potatoes", "tomatoes", "cucumbers", "zucchini", "artichokes"]
+
+lists = [list_1, list_2]
+
+print(lists)
