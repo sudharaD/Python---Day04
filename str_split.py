ea84ae5ad3acfcd37f418dd987f81023561f05fd
sudharaD <sudharad7@gmail.com>
2022-12-21 22:16:51 +0530

    string split challenge

--- str_split.py
+++ str_split.py
@@ -1,3 +1,5 @@
+import random
+
 name_string = input("Give me everybody's names, seperated by a comma. ")
 
 names = name_string.split(", ")
@@ -5,3 +7,9 @@ names = name_string.split(", ")
 print(names)
 
 print(len(names))
+
+random_name = random.randint(1, len(names))
+
+print(random_name)
+
+print(f"{names[random_name-1]} is going to buy the meal today!")
