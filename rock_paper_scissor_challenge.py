b84873d6f017abb58a8f3a4baa658ee657ac3808
sudharaD <sudharad7@gmail.com>
2022-12-22 00:39:52 +0530

    rock paper scissor challenge complete edit 1

--- rock_paper_scissor_challenge.py
+++ rock_paper_scissor_challenge.py
@@ -1,36 +1,45 @@
 import random
 import rock_paper_scissors_ascii_arts
 
-player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+while True:
+    try:
 
-rock_paper_scissor_list = [rock_paper_scissors_ascii_arts.rock, rock_paper_scissors_ascii_arts.paper, rock_paper_scissors_ascii_arts.scissors]
+        player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-print(rock_paper_scissor_list[player_choice])
+        rock_paper_scissor_list = [rock_paper_scissors_ascii_arts.rock, rock_paper_scissors_ascii_arts.paper, rock_paper_scissors_ascii_arts.scissors]
 
-print("Computer choose:")
+        print(rock_paper_scissor_list[player_choice])
 
-computer_choice = random.randint(0, 2)
+        print("Computer choose:")
 
-print(rock_paper_scissor_list[computer_choice])
+        computer_choice = random.randint(0, 2)
 
-if player_choice == 0:
-    if computer_choice == 0:
-        print("Draw!")
-    elif computer_choice == 1:
-        print("You lose!")
-    else:
-        print("You win!")
-elif player_choice == 1:
-    if computer_choice == 1:
-        print("Draw!")
-    elif computer_choice == 0:
-        print("You win!")
-    else:
-        print("You lose!")
-else:
-    if computer_choice == 2:
-        print("Draw!")
-    elif computer_choice == 0:
-        print("You lose!")
-    else:
-        print("You win!")
+        print(rock_paper_scissor_list[computer_choice])
+
+        if player_choice == 0:
+            if computer_choice == 0:
+                print("Draw!")
+            elif computer_choice == 1:
+                print("You lose!")
+            else:
+                print("You win!")
+        elif player_choice == 1:
+            if computer_choice == 1:
+                print("Draw!")
+            elif computer_choice == 0:
+                print("You win!")
+            else:
+                print("You lose!")
+        else:
+            if computer_choice == 2:
+                print("Draw!")
+            elif computer_choice == 0:
+                print("You lose!")
+            else:
+                print("You win!")
+
+        break
+
+    except:
+
+        print("Please enter valid numbers")
